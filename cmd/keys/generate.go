8b646c904a9afd41b3d7b67a9bde0f108d37059f
Yurii <yurii.vynokur@sigma.software>
2025-09-01 22:51:01 +0300

    Add generate command

---
+++ cmd/keys/generate.go
@@ -0,0 +1,14 @@
+package keys
+
+import "github.com/spf13/cobra"
+
+func init() {
+	keysCmd.AddCommand(keysGenerateCommand)
+}
+
+var keysGenerateCommand = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates key pair.",
+	Long:  `Generate an RSA key pair and store it in PEM files. The private key will be encrypted using a passphrase that you'll need to enter. AES encryption with Argon2 key derivation function is utilized.`,
+	Run:   func(cmd *cobra.Command, args []string),
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 func RootCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:   "file signer",
+		Use:   "file_signer",
 		Short: "Generate key pairs, sign files, and check signatures.",
 		Long:  "A collection of tools to generate key pairs in PEM files, sign files and verify signatures.",
 	}
